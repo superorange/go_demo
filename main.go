64c09fdf8a2deffe60bf91f0ea72e7e3015e5e90
tianjingang <tianjingang@inovance.com>
2024-05-28 17:27:06 +0800

    update-gitignore

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type Animal struct {
+	Name string
+	Age  int
+}
+
+func (animal *Animal) changeName(name string) {
+
+	animal.Name = name
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	var animal = Animal{
+		Name: "cat",
+		Age:  2,
+	}
+	animal.changeName("dog")
+	animal.changeName("Pig")
+	fmt.Printf("%+v", animal)
+
 }
